b8e2e1d22e58611e1c9f2971f05efdc004180415
M3MoN <alan.skrrrz@gmail.com>
2025-01-23 18:00:39 +0100

    Add JWT authentication support with SimpleJWT and update settings

--- server/server/settings.py
+++ server/server/settings.py
@@ -41,11 +41,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     'drf_yasg',
     'accounts.apps.AccountsConfig',
 ]
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
