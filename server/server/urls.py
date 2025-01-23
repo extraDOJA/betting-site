d08f0c5e2fbe515aae1045a496a570e9adb861b6
M3MoN <alan.skrrrz@gmail.com>
2025-01-23 17:23:59 +0100

    Add Swagger and ReDoc documentation endpoints for the API

--- server/server/urls.py
+++ server/server/urls.py
@@ -14,11 +14,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from . import views
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Betting API",
+        default_version="v1",
+        description="API documentation for the Betting application",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@betting.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('health/', views.health),
+    path("admin/", admin.site.urls),
+    path("health/", views.health),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
