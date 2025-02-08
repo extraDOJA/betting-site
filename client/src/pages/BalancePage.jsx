6da93ee5ecd1b5da03fa18534202a1b7bf358906
M3MoN <alan.skrrrz@gmail.com>
2025-02-08 14:18:04 +0100

    feat: add AddBalanceForm and BalancePage components for balance management

---
+++ client/src/pages/BalancePage.jsx
@@ -0,0 +1,21 @@
+import AddBalanceForm from "@/components/organisms/forms/AddBalanceForm";
+import Navbar from "@/components/organisms/Navbar";
+import React from "react";
+
+const BalancePage = () => {
+  return (
+    <>
+      <Navbar />
+      <main className="pt-[50px] container mx-auto">
+        <h1 className="text-center text-2xl font-bold py-3">Add Balance</h1>
+        <div className="flex justify-center px-4">
+          <div className="bg-white shadow rounded-lg p-6 max-w-[600px] w-full">
+            <AddBalanceForm />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default BalancePage;
