c99b73fb8db434cb013ee18bf581b138d9303cdf
M3MoN <alan.skrrrz@gmail.com>
2025-02-14 14:29:46 +0100

    feat: wrap HomePage in BetsProvider and integrate HomeContent component

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,21 +1,25 @@
+import HomeContent from "@/components/organisms/HomeContent";
 import LeftPanel from "@/components/organisms/LeftPanel";
 import Navbar from "@/components/organisms/Navbar";
+import { BetsProvider } from "@/context/betsContext";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div className="w-full h-screen">
       <Navbar />
-      <div className="min-w-[1024px] h-full w-full pt-[75px] grid grid-cols-8 md:grid-cols-9 ">
-        <div className="col-span-2">
+      <BetsProvider>
+      <main className="min-w-[1024px] h-full w-full pt-[75px] grid grid-cols-8 md:grid-cols-9 ">
+        <section className="col-span-2">
           <LeftPanel />
-        </div>
-        <div className="col-span-4 md:col-span-5">
-          <h1 className="text-4xl font-semibold">Home Page</h1>
-        </div>
-        <div className="col-span-2">
-        </div>
-      </div>
+        </section>
+        <section className="col-span-4 md:col-span-5">
+          <HomeContent />
+        </section>
+        <section className="col-span-2">
+        </section>
+      </main>
+      </BetsProvider>
     </div>
   );
 };
