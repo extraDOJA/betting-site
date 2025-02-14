b566688d4193588018e7d834000dbd82995e0f52
M3MoN <alan.skrrrz@gmail.com>
2025-02-14 13:02:57 +0100

    feat: add endpoint to list upcoming matches with MatchSerializer

--- server/sports/views.py
+++ server/sports/views.py
@@ -1,8 +1,9 @@
+from django.utils import timezone
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Sport, League
-from .serializers import LeagueSerializer, SportWithLeaguesSerializer
-from .swagger_docs import popular_leagues_schema, sports_with_leagues_schema
+from .models import Match, Sport, League
+from .serializers import LeagueSerializer, MatchSerializer, SportWithLeaguesSerializer
+from .swagger_docs import popular_leagues_schema, sports_with_leagues_schema, list_matches_schema
 
 
 @popular_leagues_schema
@@ -24,4 +25,15 @@ def sports_with_leagues(request):
     """
     sports = Sport.objects.filter(is_active=True).prefetch_related("leagues").filter(leagues__is_active=True).distinct().order_by("id")
     serializer = SportWithLeaguesSerializer(sports, many=True)
+    return Response(serializer.data)
+
+
+@list_matches_schema
+@api_view(["GET"])
+def list_matches(request):
+    """
+    Get list of upcoming matches
+    """
+    matches = Match.objects.filter(is_active=True, is_popular=True, start_time__gte=timezone.now()).order_by("start_time")[:15]
+    serializer = MatchSerializer(matches, many=True)
     return Response(serializer.data)
