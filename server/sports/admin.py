5d9e20bee36b72239314d4a64ce039fa2b6bb068
M3MoN <alan.skrrrz@gmail.com>
2025-02-14 13:03:01 +0100

    feat: register Match model in admin panel

--- server/sports/admin.py
+++ server/sports/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from sports.models import League, Sport
+from sports.models import League, Match, Sport
 
 # Register your models here.
 admin.site.register(Sport)
 admin.site.register(League)
+admin.site.register(Match)
