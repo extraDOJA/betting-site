7a4faad2250816f2ff9912953d02158052802f1d
M3MoN <alan.skrrrz@gmail.com>
2025-02-14 13:02:48 +0100

    feat: add Swagger documentation for listing upcoming matches

--- server/sports/swagger_docs.py
+++ server/sports/swagger_docs.py
@@ -1,17 +1,24 @@
 from drf_yasg.utils import swagger_auto_schema
 
-from sports.serializers import LeagueSerializer, SportWithLeaguesSerializer
+from sports.serializers import LeagueSerializer, MatchSerializer, SportWithLeaguesSerializer
 
 popular_leagues_schema = swagger_auto_schema(
-    method='get',
+    method="get",
     operation_description="Returns a list of active and popular leagues",
     operation_summary="List popular leagues",
-    responses={200: LeagueSerializer(many=True)}
+    responses={200: LeagueSerializer(many=True)},
 )
 
 sports_with_leagues_schema = swagger_auto_schema(
-    method='get',
+    method="get",
     operation_description="Returns a list of active sports with their active leagues",
     operation_summary="List sports with leagues",
-    responses={200: SportWithLeaguesSerializer(many=True)}
+    responses={200: SportWithLeaguesSerializer(many=True)},
+)
+
+list_matches_schema = swagger_auto_schema(
+    method="get",
+    operation_description="Returns a list of upcoming matches",
+    operation_summary="List matches",
+    responses={200: MatchSerializer(many=True)},
 )
