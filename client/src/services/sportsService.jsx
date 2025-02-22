453b3de00adbb76d611f5cf24795412fc738b606
M3MoN <alan.skrrrz@gmail.com>
2025-02-22 11:26:42 +0100

    feat: add createBetSlip method and improve formatting in sportsService

--- client/src/services/sportsService.jsx
+++ client/src/services/sportsService.jsx
@@ -3,16 +3,21 @@ import axios from "axios";
 const API_URL = "sports/api";
 
 export const fetchPopularLeagues = async () => {
-    const response = await axios.get(`${API_URL}/leagues/popular/`);
-    return response.data;
-}
+  const response = await axios.get(`${API_URL}/leagues/popular/`);
+  return response.data;
+};
 
 export const fetchSportsWithLeagues = async () => {
-    const response = await axios.get(`${API_URL}/leagues/`);
-    return response.data;
-}
+  const response = await axios.get(`${API_URL}/leagues/`);
+  return response.data;
+};
 
 export const fetchPopularMatches = async () => {
-    const response = await axios.get(`${API_URL}/matches/popular/`);
-    return response.data;
-}
+  const response = await axios.get(`${API_URL}/matches/popular/`);
+  return response.data;
+};
+
+export const createBetSlip = async (betSlipData) => {
+  const response = await axios.post(`${API_URL}/bet/`, betSlipData);
+  return response.data;
+};
