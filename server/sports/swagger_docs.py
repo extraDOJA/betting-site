d3d00ea25f34806ffaa4b73370c77d8f7843e094
M3MoN <alan.skrrrz@gmail.com>
2025-02-22 11:27:52 +0100

    feat: add create_bet_slip endpoint with validation and user balance checks

--- server/sports/swagger_docs.py
+++ server/sports/swagger_docs.py
@@ -1,6 +1,6 @@
 from drf_yasg.utils import swagger_auto_schema
 
-from sports.serializers import LeagueSerializer, MatchSerializer, SportWithLeaguesSerializer
+from sports.serializers import BetSlipCreateSerializer, LeagueSerializer, MatchSerializer, SportWithLeaguesSerializer
 
 popular_leagues_schema = swagger_auto_schema(
     method="get",
@@ -21,4 +21,12 @@ list_matches_schema = swagger_auto_schema(
     operation_description="Returns a list of upcoming matches",
     operation_summary="List matches",
     responses={200: MatchSerializer(many=True)},
+)
+
+bet_slip_create_schema = swagger_auto_schema(
+    method="post",
+    operation_description="Create a bet slip",
+    operation_summary="Create bet slip",
+    request_body= BetSlipCreateSerializer,
+    responses={200: BetSlipCreateSerializer},
 )
--- server/sports/urls.py
+++ server/sports/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('leagues/', views.sports_with_leagues, name='sports-with-leagues'),
     path('leagues/popular/', views.popular_leagues, name='popular-leagues'),
     path('matches/popular/', views.list_popular_matches, name='list-popular-matches'),
+    path('bet/', views.create_bet_slip, name='create-bet-slip'),
 ]
