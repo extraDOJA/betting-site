ab5559f4229f0b7d14179d2b272254836875fd91
M3MoN <alan.skrrrz@gmail.com>
2025-03-03 18:54:12 +0100

    feat: adjust loading skeleton height in UserBetsList component for improved UI

--- client/src/components/organisms/UserBetsList.jsx
+++ client/src/components/organisms/UserBetsList.jsx
@@ -5,9 +5,9 @@ import UserBetCard from "./UserBetCard";
 const UserBetsList = ({ bets, isLoading, emptyMessage }) => {
   if (isLoading) {
     return (
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+      <div className="grid grid-cols-1 gap-4 mt-4">
         {[...Array(3)].map((_, i) => (
-          <Skeleton key={i} className="h-[200px] w-full rounded-md" />
+          <Skeleton key={i} className="h-[120px] w-full rounded-md" />
         ))}
       </div>
     );
