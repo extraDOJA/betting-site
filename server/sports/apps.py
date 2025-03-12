2c682b7066da63eda6af82e2b6f82f513858ba4e
M3MoN <alan.skrrrz@gmail.com>
2025-03-12 18:50:48 +0100

    feat: add signal handling for match result updates to settle bets automatically

--- server/sports/apps.py
+++ server/sports/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SportsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sports'
+
+    def ready(self):
+        import sports.signals  # noqa: F401
