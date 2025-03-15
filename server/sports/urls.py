65893b33e0776f49fdec204fbf3c8f4f0e97d9bf
M3MoN <alan.skrrrz@gmail.com>
2025-03-15 18:47:30 +0100

    feat: add league details and matches endpoints with serializers

--- server/sports/urls.py
+++ server/sports/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('leagues/', views.sports_with_leagues, name='sports-with-leagues'),
     path('leagues/popular/', views.popular_leagues, name='popular-leagues'),
+    path("leagues/<str:league_slug>/", views.league_details, name="league-detail"),
+    path("leagues/<str:league_slug>/matches/", views.league_matches, name="league-matches"),
     path('matches/popular/', views.list_popular_matches, name='list-popular-matches'),
     path('bet/', views.create_bet_slip, name='create-bet-slip'),
     path('bets/', views.user_bet_slips, name='user-bet-slips'),
