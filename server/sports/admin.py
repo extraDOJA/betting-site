a4fa5475a9d2ab39ba2a269ef004654bbd3fe6e3
M3MoN <alan.skrrrz@gmail.com>
2025-03-29 12:07:18 +0100

    feat: add search functionality to Match admin interface

--- server/sports/admin.py
+++ server/sports/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 
 from sports.models import Bet, BetSlip, League, Match, Sport
 
+
+class MatchAdmin(admin.ModelAdmin):
+    search_fields = ['home_team', 'away_team']
+
+
 # Register your models here.
 admin.site.register(Sport)
 admin.site.register(League)
-admin.site.register(Match)
+admin.site.register(Match, MatchAdmin)
 admin.site.register(BetSlip)
 admin.site.register(Bet)
