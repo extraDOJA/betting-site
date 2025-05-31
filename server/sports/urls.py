fc68a77277f6a9a9130708df18eb22f1954adde3
M3MONs <alan.skrrrz@gmail.com>
2025-05-31 17:37:12 +0200

    feat: add route for single_match to retrieve details of a specific match

--- server/sports/urls.py
+++ server/sports/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("leagues/<str:league_slug>/", views.league_details, name="league-detail"),
     path("leagues/<str:league_slug>/matches/", views.league_matches, name="league-matches"),
     path('matches/popular/', views.list_popular_matches, name='list-popular-matches'),
+    path('matches/<int:match_id>/', views.single_match, name='single-match'),
     path('bet/', views.create_bet_slip, name='create-bet-slip'),
     path('bets/', views.user_bet_slips, name='user-bet-slips'),
     path('bets/validate/', views.validate_bets_availability, name='validate-bet-slip'),
