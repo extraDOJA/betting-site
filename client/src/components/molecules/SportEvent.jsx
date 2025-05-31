098203acf6c7c4a8adb2e1d8e7b0a840ee1509a4
M3MONs <alan.skrrrz@gmail.com>
2025-05-31 17:38:48 +0200

    feat: update SportEvent component to use Link for navigation to match details

--- client/src/components/molecules/SportEvent.jsx
+++ client/src/components/molecules/SportEvent.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { formatMatchDate } from "@/utils/formatMatchDate";
 import { useBets } from "@/context/betsContext";
 import BetButton from "@/components/atoms/Bet/BetButton";
+import { Link } from "react-router";
 
 const SportEvent = ({ match }) => {
   const { selectedBets, toggleBet } = useBets();
@@ -11,11 +12,11 @@ const SportEvent = ({ match }) => {
     <div className="bg-card rounded-md py-3 px-4 mb-3 text-xs lg:text-base xl:text-lg" style={{ fontFamily: "Helvetica" }}>
       <p className="text-xs text-gray-700 font-medium">{match.queue}</p>
       <div className="flex justify-between items-center">
-        <p className="py-5 flex gap-4 items-center">
+        <Link to={`/match/${match.id}`} as="p" className="py-5 flex gap-4 items-center">
           <span className="font-bold">{match.home_team}</span>
           <span className="flex flex-col items-center text-xs text-gray-500">{formatMatchDate(match.start_time)}</span>
           <span className="font-bold">{match.away_team}</span>
-        </p>
+        </Link>
         <div className="flex gap-2">
           <BetButton
             title={match.home_team}
