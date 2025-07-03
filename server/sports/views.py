80974c2640b0b01c025eb5870d6e2fba59f8980d
Alan Skrzypczak <alan.skrrrz@gmail.com>
2025-07-03 19:28:54 +0200

    fix: enhance bet slip creation to check user balance and match availability

--- server/sports/views.py
+++ server/sports/views.py
@@ -82,16 +82,24 @@ def create_bet_slip(request) -> Response:
     if not serializer.is_valid():
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    if request.user.balance < serializer.validated_data["total_amount"]:
+            return Response({"error": "Insufficient balance"}, status=status.HTTP_400_BAD_REQUEST)
+
     try:
-        bet_slip, user_balance = BetSlipRepository.create_bet_slip(user=request.user, validated_data=serializer.validated_data)
-    except InsufficientBalanceError:
-        return Response({"error": "Insufficient balance"}, status=status.HTTP_400_BAD_REQUEST)
+        # TODO: Fix repository for creating bet slips
+        for bet_data in serializer.validated_data["bets"]:
+            match = bet_data["match"]
+            if not match.can_bet:
+                return Response({"error": f"Match {match} is not available for betting"}, status=status.HTTP_400_BAD_REQUEST)
+
+        request.user.substrat_balance(serializer.validated_data["total_amount"])
+        bet_slip = serializer.save(user=request.user)
     except MatchNotAvailableError as e:
         return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
     response_serializer = BetSlipResponseSerializer(bet_slip)
     response_data = response_serializer.data
-    response_data["user_balance"] = user_balance
+    response_data["user_balance"] = request.user.balance
 
     return Response(response_data, status=status.HTTP_201_CREATED)
 
